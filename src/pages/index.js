2811f528ad985a57dc85389fc59b3b430d742c8b
bdiazc90 <bdiazc90@gmail.com>
2022-08-04 20:41:23 -0500

    Reto 1 done

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
 export { default as MainView } from "./Main";
 export { default as SearchView } from "./Search";
 export { default as SearchResultsView } from "./SearchResults";
+export { default as LoginView } from "./Login";
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { MainView, SearchView, SearchResultsView } from "../pages";
+import { LoginView, SearchView, SearchResultsView } from "../pages";
 
 const Router = () => {
 	return (
 		<BrowserRouter>
 			<Routes>
-				<Route path="/" element={<MainView />} />
+				<Route path="/" element={<LoginView />} />
 				<Route path="/search" element={<SearchView />} />
 				<Route
 					path="/search/:searchText"
