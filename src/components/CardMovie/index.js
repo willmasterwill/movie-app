2cabd64ab047bea3d667a9dc8acefd3d78cd139a
linder3hs <linderhassinger00@gmail.com>
2022-08-05 20:32:59 -0500

    chore: cards

--- src/components/CardMovie/index.js
+++ src/components/CardMovie/index.js
@@ -1,52 +1,52 @@
 import {
-	Avatar,
-	Box,
-	Card,
-	CardContent,
-	Grid,
-	Typography,
-	Chip,
+  Card,
+  CardContent,
+  CardMedia,
+  Grid,
+  Typography,
+  Chip,
 } from "@mui/material";
 
 const CardMovie = ({ movie }) => {
-	return (
-		<Box my={3}>
-			<Card>
-				<CardContent
-					sx={{
-						cursor: "pointer",
-					}}
-				>
-					<Grid container spacing={3}>
-						<Grid item xs={2}>
-							<Avatar
-								sx={{ width: 100, height: 100 }}
-								src={movie.Poster}
-								variant="rounded"
-							/>
-						</Grid>
-						<Grid item xs={10}>
-							<Typography
-								variant="h6"
-								sx={{ fontWeight: "bold" }}
-							>
-								{movie.Title}
-							</Typography>
-							<Typography variant="body1">
-								{movie.Year}
-							</Typography>
-							<Chip
-								label={movie.Type}
-								color="success"
-								size="small"
-								variant="outlined"
-							/>
-						</Grid>
-					</Grid>
-				</CardContent>
-			</Card>
-		</Box>
-	);
+  return (
+    <Grid item xs={12} md={4} my={3}>
+      <Card
+        sx={{
+          height: 400,
+        }}
+      >
+        <CardMedia
+          component="img"
+          height="240"
+          sx={{
+            objectPosition: "top",
+          }}
+          image={movie.Poster}
+          alt="green iguana"
+        />
+        <CardContent
+          sx={{
+            cursor: "pointer",
+          }}
+        >
+          <Grid container spacing={3}>
+            <Grid item xs={12}>
+              <Typography variant="h6" sx={{ fontWeight: "bold" }}>
+                {movie.Title}
+              </Typography>
+              <Typography variant="body1">{movie.Year}</Typography>
+              <Chip
+                label={movie.Type}
+                color="success"
+                size="small"
+                variant="outlined"
+              />
+            </Grid>
+          </Grid>
+        </CardContent>
+      </Card>
+    </Grid>
+  );
 };
 
 export default CardMovie;
--- src/pages/SearchResults/index.js
+++ src/pages/SearchResults/index.js
@@ -1,54 +1,47 @@
 import { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-
-import { Box, Container, IconButton } from "@mui/material";
-
-import CloseIcon from "@mui/icons-material/Close";
-
+import { Box, Container, Grid, IconButton, Typography } from "@mui/material";
 import Services from "../../services";
-import { CardMovie, MenuBar, MenuBarChildren } from "../../components";
+import { CardMovie } from "../../components";
+import ArrowBackRoundedIcon from "@mui/icons-material/ArrowBackRounded";
 
 const SearchResults = () => {
-	const [movies, setMovies] = useState([]);
-	const { searchText } = useParams();
-	const history = useNavigate();
+  const [movies, setMovies] = useState([]);
+  const { searchText } = useParams();
+  const history = useNavigate();
 
-	async function getSearchResults() {
-		const data = await Services.searchByText(searchText);
-		setMovies(data.Search);
-		console.log(data.Search);
-	}
+  async function getSearchResults() {
+    const data = await Services.searchByText(searchText);
+    setMovies(data.Search);
+    console.log(data.Search);
+  }
 
-	useEffect(() => {
-		getSearchResults();
-	}, []);
+  useEffect(() => {
+    getSearchResults();
+  }, []);
 
-	function backButton() {
-		history("/search");
-	}
+  function backButton() {
+    history("/search");
+  }
 
-	return (
-		<Box>
-			<MenuBar
-				text={"Search Results: " + searchText}
-				buttonClick={backButton}
-				buttonIcon={<CloseIcon />}
-			/>
-
-			<MenuBarChildren text={"Search Results: " + searchText}>
-				<IconButton color="info" onClick={backButton}>
-					<CloseIcon />
-				</IconButton>
-			</MenuBarChildren>
-
-			<Container>
-				{movies.length > 0 &&
-					movies.map((movie, index) => (
-						<CardMovie movie={movie} key={index} />
-					))}
-			</Container>
-		</Box>
-	);
+  return (
+    <Box>
+      <Container>
+        <IconButton onClick={backButton}>
+          <ArrowBackRoundedIcon color="success" />
+        </IconButton>
+        <Grid container spacing={3}>
+          <Grid item xs={12}>
+            <Typography variant="h6">Search / {searchText}</Typography>
+          </Grid>
+          {movies.length > 0 &&
+            movies.map((movie, index) => (
+              <CardMovie movie={movie} key={index} />
+            ))}
+        </Grid>
+      </Container>
+    </Box>
+  );
 };
 
 export default SearchResults;
